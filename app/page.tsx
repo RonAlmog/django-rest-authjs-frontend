34cf9dee981951a09d23254c665939fd525aaa1b
Ron Almog <ron8132@gmail.com>
2023-12-10 13:27:45 -0500

    add next auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -5,7 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+      <Button
+        size="sm"
+        variant="outline"
+        onClick={() => console.log("Beep boop")}
+      >
+        Sign in
+      </Button>
     </main>
   );
 }
