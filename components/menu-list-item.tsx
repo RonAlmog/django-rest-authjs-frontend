8dd2667e9ed02e248a3f59720bde9ba3a8bea351
Ron Almog <ron8132@gmail.com>
2023-12-14 11:48:56 -0500

    add categories popup menu

---
+++ components/menu-list-item.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+type Props = {};
+
+const MenuListItem = (props: Props) => {
+  return <div>MenuListItem</div>;
+};
+
+export default MenuListItem;
--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -8,9 +8,11 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import React from "react";
 
 const NavMenu = () => {
   const router = useRouter();
@@ -19,6 +21,39 @@ const NavMenu = () => {
       router.push("/"); // Redirect to the dashboard page after signing out
     });
   };
+
+  const categories: { title: string; href: string; description: string }[] = [
+    {
+      title: "Beach",
+      href: "/properties/category/beach",
+      description: "This property is close to the beach!",
+    },
+    {
+      title: "Windmills",
+      href: "/properties/category/windmills",
+      description: "This property has windmills!",
+    },
+    {
+      title: "Modern",
+      href: "/properties/category/modern",
+      description: "This property is modern!",
+    },
+    {
+      title: "Coutryside",
+      href: "/properties/category/countryside",
+      description: "This property is in the coutryside!",
+    },
+    {
+      title: "Pool",
+      href: "/properties/category/pool",
+      description: "This property is has a pool",
+    },
+    {
+      title: "Islands",
+      href: "/properties/category/islands",
+      description: "This property is on an island!",
+    },
+  ];
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -29,6 +64,20 @@ const NavMenu = () => {
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Categories</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {categories.map((cat) => (
+                <ListItem key={cat.title} title={cat.title} href={cat.href}>
+                  {cat.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
         <NavigationMenuItem>
           <Link href="/signup" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
@@ -53,9 +102,56 @@ const NavMenu = () => {
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/server" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Server
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/client" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Client
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/extra" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Extra
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
   );
 };
 
 export default NavMenu;
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
